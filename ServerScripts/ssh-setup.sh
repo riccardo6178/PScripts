4fd12c1aa44079642a9b1b951fad04642ca1cb8e
riccardo6178 <r.riccardo1210@gmail.com>
2026-06-25 22:28:15 +0200

    delete: deleted comments from ssh-setup

--- ServerScripts/ssh-setup.sh
+++ ServerScripts/ssh-setup.sh
@@ -5,13 +5,13 @@ if [ "$EUID" -ne 0 ]; then
     echo "Eseguire come root" >&2
     exit 1
 fi
-# Backup e disabilita password auth
+
 cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
 sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication no/' /etc/ssh/sshd_config
 grep -q '^PasswordAuthentication' /etc/ssh/sshd_config || echo 'PasswordAuthentication no' >> /etc/ssh/sshd_config
 sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config
 grep -q '^PermitRootLogin' /etc/ssh/sshd_config || echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config
-# Prepara authorized_keys
+
 if [ -d /root/.ssh ]; then
 	touch /root/.ssh/authorized_keys
 	chmod 600 /root/.ssh/authorized_keys
